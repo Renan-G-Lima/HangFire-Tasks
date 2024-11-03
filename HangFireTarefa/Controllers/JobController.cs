f14c731d84020052aaa50b4469f8a20a49cb2745
rGabriel <126360827+rGabrielLima@users.noreply.github.com>
2024-11-03 10:35:18 -0300

    Aplicativo usando hang fire para testar suas funcionalidades

---
+++ HangFireTarefa/Controllers/JobController.cs
@@ -0,0 +1,69 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireTarefa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpGet]
+        public void ListInt()
+        {
+            for(int i = 0; i < 1; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateBackgroundJob")]
+        public ActionResult CreateBackgroundJob()
+        {
+            BackgroundJob.Enqueue(() => ListInt());
+            return Ok();
+        }
+
+
+
+        [HttpPost]
+        [Route("CreateScheduleJob")]
+        public ActionResult CreateScheduleJob()
+        {
+            var scheduleDateTime = DateTime.UtcNow.AddSeconds(5);
+            var dateTimeOffSet = new DateTimeOffset(scheduleDateTime);
+
+            BackgroundJob.Schedule(() => Console.WriteLine("agendamento"), dateTimeOffSet);
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [Route("CreateContinuationJob")]
+        public ActionResult CreateContinuationJob()
+        {
+            var scheduleDateTime = DateTime.UtcNow.AddSeconds(5);
+            var dateTimeOffSet = new DateTimeOffset(scheduleDateTime);
+
+            var job1 = BackgroundJob.Schedule(() => Console.WriteLine("schedule"), dateTimeOffSet);
+            var job2 = BackgroundJob.ContinueJobWith(job1, () => Console.WriteLine("second job"));
+            var job3 = BackgroundJob.ContinueJobWith(job2, () => Console.WriteLine("third job"));
+            var job4 = BackgroundJob.ContinueJobWith(job3, () => Console.WriteLine("fourth job"));
+
+            return Ok();
+        }
+
+
+
+        [HttpPost]
+        [Route("CreateRecurringJob")]
+        public ActionResult CreateRecurringJob()
+        {
+            RecurringJob.AddOrUpdate("RecurringJob1", () => Console.WriteLine("recurring job"), "* * * * *");
+
+            return Ok();
+        }
+    }
+}
